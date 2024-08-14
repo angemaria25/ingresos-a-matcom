d9a5f7a08073bd7637996a0d9eeef9dc08c2f331
angemaria25 <angelicamariamartinezcespedes@gmail.com>
2024-08-14 07:52:51 -0400

    agregando modifiaciones a la página

--- proyecto.py
+++ proyecto.py
@@ -6,10 +6,8 @@ data = pd.read_json("datos\datos_clasificados.json")
 
 # Eliminar espacios en blanco al principio y al final de los nombres de las columnas
 data.columns = data.columns.str.strip()
-
 # Eliminar espacios en blanco al principio y al final de todas las celdas
 data = data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-
 # Configurar pandas para mostrar todas las filas y columnas
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -17,9 +15,10 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.expand_frame_repr', False)
 
 st.markdown("# Ingresos a la Facultad de Matemática y Computación.")
-#######################################################################################################################
-st.write("### Ingresos a MATCOM a lo largo del tiempo.")
 
+#########################################################
+st.write("### Ingresos a MATCOM a lo largo del tiempo.")
+#########################################################
 opcion_visualizacion = st.selectbox('Seleccione el tipo de visualización:', ['Total de Inscripciones', 'Inscripciones por Género', 'Inscripciones por Provincia', 'Inscripciones por Vía de Ingreso'])
 
 inscripciones_por_curso = data.groupby('Curso').size().reset_index(name='Número de Inscripciones')
@@ -82,11 +81,30 @@ elif opcion_visualizacion == 'Inscripciones por Vía de Ingreso':
         yaxis=dict(showgrid=False),
         legend_title_text='Vía de Ingreso')
     st.plotly_chart(fig04)
+    
+    data_preuniversitario = data.dropna(subset=['Vía Ingreso'])
+
+    data_preuniversitario = data_preuniversitario[data_preuniversitario['Vía Ingreso'].str.contains('INSTITUTOS PREUNIVERSITARIOS', na=False)]
+
+    distribucion_pre = data_preuniversitario.groupby(['Curso', 'Tipo de Pre']).size().reset_index(name='Número de Estudiantes')
+        
+    fig05 = px.bar(distribucion_pre, 
+                            x='Curso', 
+                            y='Número de Estudiantes', 
+                            color='Tipo de Pre', 
+                            barmode='group',
+                            title='Distribución de Estudiantes de Preuniversitario según el Tipo de Pre.',
+                            labels={'Número de Estudiantes':'Número de Estudiantes', 'Curso':'Curso'})
+    fig05.update_traces(marker=dict(line=dict(color='#000000', width=2)))
+    fig05.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False), legend_title_text='Tipo de Pre')
+    st.plotly_chart(fig05)
 else:
     st.plotly_chart(fig01)
-#####################################################################################################################
-st.write("### Ingresos por Carrera y Curso.")
 
+
+##############################################
+st.write("### Ingresos por Carrera y Curso.")
+##############################################
 opcion_seleccionada = st.radio("Seleccione la opción deseada:",("Ingresos por Carrera y Curso.", "Distribución por Vía de Ingreso.", "Ingresos por Provincias."))
 
 if opcion_seleccionada == "Ingresos por Carrera y Curso.":
@@ -139,13 +157,9 @@ elif opcion_seleccionada == "Distribución por Vía de Ingreso.":
                         marker=dict(line=dict(color='#000000', width=2)))
     fig2.update_layout(legend_title_text='Vías de Ingreso:')
     st.plotly_chart(fig2)
-#########################################################################################################################
-st.write("### Ingresos por Provincias.")
-
-
-#########################################################################################################################
+#################################
 st.write("### Bajas generales.")
-
+#################################
 bajas = data[data['Estado'] == 'Baja']
 
 opcion_visualizacion = st.selectbox('Seleccione la opción deseada:',['Bajas Totales', 'Bajas por Género', 'Bajas por Provincia', 'Bajas por Vía de Ingreso'])
@@ -209,71 +223,95 @@ elif opcion_visualizacion == 'Bajas por Vía de Ingreso':
         yaxis=dict(showgrid=False),
         legend_title_text='Vía de Ingreso')
     st.plotly_chart(fig14)
+    
+    bajas_preuniversitarios = bajas[bajas['Vía Ingreso'].str.contains('INSTITUTOS PREUNIVERSITARIOS', na=False)]
+    bajas_por_tipo_pre = bajas_preuniversitarios.groupby(['Curso', 'Tipo de Pre']).size().reset_index(name='Número de Bajas')
+    
+    fig15 = px.bar(bajas_por_tipo_pre, 
+                    x='Curso', 
+                    y='Número de Bajas', 
+                    color='Tipo de Pre', 
+                    barmode='group',
+                    title='Bajas por Tipo de Preuniversitario y Curso.',
+                    labels={'Número de Bajas':'Número de Bajas', 'Curso':'Curso', 'Tipo de Pre':'Tipo de Pre'})
+    
+    fig15.update_traces(marker=dict(line=dict(color='#000000', width=2)))
+    fig15.update_layout(
+        xaxis=dict(showgrid=False),  
+        yaxis=dict(showgrid=False),
+        legend_title_text='Tipo de Pre')
+    st.plotly_chart(fig15)
 else:
     st.plotly_chart(fig11)
-#########################################################################################################################
+###########################################
 st.write("### Bajas por Curso y Carrera.")
+###########################################
+opciones = st.radio("Seleccione una opción:",("Bajas por Carrera y Curso.", "Bajas por Vía de Ingreso."))
 
 bajas = data[data['Estado'] == 'Baja']
 
-cursos = data['Curso'].unique()
-cursos_select = st.multiselect('Selecciona los Cursos:', cursos, default=[], placeholder='Cursos')
-bajas = bajas[bajas['Curso'].isin(cursos_select) | (cursos_select == [])]
+if opciones == "Bajas por Carrera y Curso.":
+    cursos = data['Curso'].unique()
+    cursos_select = st.multiselect('Selecciona los Cursos:', cursos, default=[], placeholder='Cursos')
+    bajas = bajas[bajas['Curso'].isin(cursos_select) | (cursos_select == [])]
 
-bajas_por_carrera_curso = bajas.groupby(['Carrera', 'Curso']).size().reset_index(name='Número de Bajas')
-bajas_totales_por_carrera = bajas.groupby('Carrera').size().reset_index(name='Número de Bajas')
-fig3 = px.bar(bajas_por_carrera_curso, 
-                y='Número de Bajas', 
-                x='Curso', 
-                color='Carrera', 
-                barmode='group', 
-                title='Número de Bajas por Carrera y Curso.',
-                labels={'Número de Bajas': 'Número de Bajas', 'Carrera': 'Carrera'},
-                color_discrete_sequence=px.colors.qualitative.Plotly)
-fig3.update_traces(marker=dict(line=dict(color='#000000', width=2)))
-fig3.update_layout(
-    xaxis=dict(showgrid=False),  
-    yaxis=dict(showgrid=False),
-    legend_title_text='Carreras:'
-)
-st.plotly_chart(fig3)
+    bajas_por_carrera_curso = bajas.groupby(['Carrera', 'Curso']).size().reset_index(name='Número de Bajas')
+    bajas_totales_por_carrera = bajas.groupby('Carrera').size().reset_index(name='Número de Bajas')
+    fig3 = px.bar(bajas_por_carrera_curso, 
+                    y='Número de Bajas', 
+                    x='Curso', 
+                    color='Carrera', 
+                    barmode='group', 
+                    title='Número de Bajas por Carrera y Curso.',
+                    labels={'Número de Bajas': 'Número de Bajas', 'Carrera': 'Carrera'},
+                    color_discrete_sequence=px.colors.qualitative.Plotly)
+    fig3.update_traces(marker=dict(line=dict(color='#000000', width=2)))
+    fig3.update_layout(
+        xaxis=dict(showgrid=False),  
+        yaxis=dict(showgrid=False),
+        legend_title_text='Carreras:'
+    )
+    st.plotly_chart(fig3)
+elif opciones == "Bajas por Vía de Ingreso.":
+    st.write("### Vía de ingreso de las bajas.")
+    st.write("### La mayor cantidad de bjas son de Pre , en el caso de matematica los que se quedan sin carrera se les otorga matematica")
+    
+    cursos = data['Curso'].unique()
 
+    carreras = data['Carrera'].unique()
 
-st.write("### MOTIVOS DE LAS BAJAS")
+    selected_carrera = st.radio('Selecciona una Carrera:', carreras)
+
+    selected_curso = st.selectbox('Selecciona un Curso:', cursos)
+
+    df_filtrado = bajas[(bajas['Curso'] == selected_curso) & (bajas['Carrera'] == selected_carrera)]
+
+    via_ingreso_counts = df_filtrado.groupby(['Vía Ingreso', 'Carrera', 'Curso']).size().reset_index(name='Número de Bajas')
+
+    fig4 = px.bar(via_ingreso_counts, 
+                    x='Vía Ingreso', 
+                    y='Número de Bajas', 
+                    color='Carrera', 
+                    barmode='group', 
+                    title=f'Número de Bajas por Vía de Ingreso - Curso: {selected_curso} - Carrera: {selected_carrera}',
+                    labels={'Número de Bajas': 'Número de Bajas', 'Vía Ingreso': 'Vía de Ingreso'},
+                    color_discrete_sequence=px.colors.qualitative.Plotly)
+    fig4.update_traces(marker=dict(line=dict(color='#000000', width=2)))
+    fig4.update_layout(
+        xaxis=dict(showgrid=False),  
+        yaxis=dict(showgrid=False),
+        legend_title_text='Carreras:'
+    )
+    st.plotly_chart(fig4)
+    
+
+st.write("### MOTIVOS DE LAS BAJAS???")
 st.write("### Xq piden la baja??En que opcion la cojieron??q promedio tenian??.")
-#######################################################################################################################
-st.write("### Vía de ingreso de las bajas.")
-st.write("### La mayor cantidad de bjas son de Pre , en el caso de matematica los que se quedan sin carrera se les otorga matematica")
-
-cursos = data['Curso'].unique()
-
-carreras = data['Carrera'].unique()
-
-selected_carrera = st.radio('Selecciona una Carrera:', carreras)
-
-selected_curso = st.selectbox('Selecciona un Curso:', cursos)
-
-df_filtrado = bajas[(bajas['Curso'] == selected_curso) & (bajas['Carrera'] == selected_carrera)]
-
-via_ingreso_counts = df_filtrado.groupby(['Vía Ingreso', 'Carrera', 'Curso']).size().reset_index(name='Número de Bajas')
-
-fig4 = px.bar(via_ingreso_counts, 
-                x='Vía Ingreso', 
-                y='Número de Bajas', 
-                color='Carrera', 
-                barmode='group', 
-                title=f'Número de Bajas por Vía de Ingreso - Curso: {selected_curso} - Carrera: {selected_carrera}',
-                labels={'Número de Bajas': 'Número de Bajas', 'Vía Ingreso': 'Vía de Ingreso'},
-                color_discrete_sequence=px.colors.qualitative.Plotly)
-fig4.update_traces(marker=dict(line=dict(color='#000000', width=2)))
-fig4.update_layout(
-    xaxis=dict(showgrid=False),  
-    yaxis=dict(showgrid=False),
-    legend_title_text='Carreras:'
-)
-st.plotly_chart(fig4)
-
 st.write("### De los de Pre piden mas la baja los de ipcv o los del pre de la calle??")
+st.write("### Motivo de las bajas de Pre?? En que opción cojieron la carrera???de donde vienen??TIENEN ALGO EN COMÚN LAS BAJAS???")
+st.write("### Los que mas piden la baja son de Provincia??")
 st.write("### Cuantos piden la baja de provincia??")
 st.write("### de que via de ingreso vienen los estudiantes de provincia")
-#######################################################################################################################
+st.write("### Existe alguna correlación entre los estudiantes que piden la baja con la opcion??")
+st.write("### Cantidad de repitentes?de reingresos?")
+st.write("### Como ha variado el promedio del escalafón para el ingreso a las diferentes carreras a lo largo de los años??")
